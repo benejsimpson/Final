009925d5b5d69aa5e37d82e36e1f768efbd4df38
benejsimpson <143635867+benejsimpson@users.noreply.github.com>
2024-09-05 13:15:50 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,6 +1,6 @@
 <?php
     $errors = '';
-    $myemail = 'b.simpson051102@gmail.com';//<-----Put Your email address here.
+    $myemail = '';//<-----Put Your email address here.
     if(empty($_POST['name'])  ||
        empty($_POST['email']) ||
        empty($_POST['message']))
